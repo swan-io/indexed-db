4033feefc2f3cfd050c5ce1154676b0e3637e878
Mathieu Acthernoene <zoontek@gmail.com>
2023-04-24 19:05:35 +0200

    WIP retry

--- src/retry.ts
+++ src/retry.ts
@@ -1,28 +1,14 @@
 import { Future, Result } from "@swan-io/boxed";
 
-class RetryAbortedError<E> extends Error {
-  constructor(public originalError: E) {
-    super("Attempt has been aborted");
-    this.name = this.constructor.name;
-    Object.setPrototypeOf(this, RetryAbortedError.prototype);
-  }
-}
-
-const resolveAfter = (ms: number): Future<void> =>
-  Future.make((resolve) => {
-    setTimeout(resolve, ms);
-  });
-
 export type RetryConfig = {
   maxAttempts?: number;
-  delay?: number;
-  onFailedAttempt?: () => Future<void> | void;
+  onFailedAttempt?: () => void;
 };
 
-export const retry = <T, E>(
-  getFuture: (/*abort: () => void*/) => Future<Result<T, E>>,
-  { maxAttempts = 3, delay = 1000, onFailedAttempt }: RetryConfig = {},
-): Future<Result<T, E>> => {
+export const retry = <A, E>(
+  getFuture: () => Future<Result<A, E>>,
+  { maxAttempts = 3, onFailedAttempt }: RetryConfig = {},
+): Future<Result<A, E>> => {
   const safeMaxAttempts = Math.max(maxAttempts, 1);
 
   return getFuture().flatMapError((error) => {
@@ -30,17 +16,11 @@ export const retry = <T, E>(
       return Future.value(Result.Error(error));
     }
 
-    const tmp = onFailedAttempt?.();
-    const x = tmp instanceof Future ? tmp : Future.value(tmp);
+    onFailedAttempt?.();
 
-    return x
-      .flatMap(() => resolveAfter(delay))
-      .flatMap(() =>
-        retry(getFuture, {
-          maxAttempts: safeMaxAttempts - 1,
-          delay,
-          onFailedAttempt,
-        }),
-      );
+    return retry(getFuture, {
+      maxAttempts: safeMaxAttempts - 1,
+      onFailedAttempt,
+    });
   });
 };
