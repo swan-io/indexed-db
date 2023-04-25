402f680b2d35b34c5122c4648f0df89974a42715
Mathieu Acthernoene <zoontek@gmail.com>
2023-04-25 17:50:45 +0200

    Setup vitest for unit tests

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    environment: "jsdom",
+    include: ["./tests/unit/**/*.test.ts"],
+    setupFiles: ["fake-indexeddb/auto"],
+    watch: false,
+    threads: false,
+  },
+});
