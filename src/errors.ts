de0c8fdbedff63c5f29a0173f504b92b64149963
Mathieu Acthernoene <zoontek@gmail.com>
2023-04-27 12:03:54 +0200

    Copy original error stack

--- src/errors.ts
+++ src/errors.ts
@@ -1,5 +1,13 @@
 import { iOSVersion } from "./safari";
 
+const mergeErrors = (errors: { original: Error; new: Error }) => {
+  if (errors.original.stack != null) {
+    errors.new.stack = errors.original.stack;
+  }
+
+  return errors.new;
+};
+
 export const rewriteError = (error: DOMException | null): Error => {
   if (error == null) {
     return new Error("Unknown IndexedDB error");
@@ -11,22 +19,28 @@ export const rewriteError = (error: DOMException | null): Error => {
       "An internal error was encountered in the Indexed Database server";
 
     if (error.message.indexOf(IOS_ERROR) >= 0) {
-      return new Error(
-        `IndexedDB has thrown '${IOS_ERROR}'. ` +
-          `This is likely due to an unavoidable bug in iOS ` +
-          `(https://bugs.webkit.org/show_bug.cgi?id=197050).`,
-      );
+      return mergeErrors({
+        original: error,
+        new: new Error(
+          `IndexedDB has thrown '${IOS_ERROR}'. ` +
+            `This is likely due to an unavoidable bug in iOS ` +
+            `(https://bugs.webkit.org/show_bug.cgi?id=197050).`,
+        ),
+      });
     }
   }
 
   // https://github.com/firebase/firebase-js-sdk/blob/firebase%409.20.0/packages/firestore/src/local/simple_db.ts#L335
   if (error.name === "InvalidStateError") {
-    return new Error(
-      `Unable to open an IndexedDB connection. ` +
-        `This could be due to running in a private browsing ` +
-        `session on a browser whose private browsing ` +
-        `sessions do not support IndexedDB: ${error.message}`,
-    );
+    return mergeErrors({
+      original: error,
+      new: new Error(
+        `Unable to open an IndexedDB connection. ` +
+          `This could be due to running in a private browsing ` +
+          `session on a browser whose private browsing ` +
+          `sessions do not support IndexedDB: ${error.message}`,
+      ),
+    });
   }
 
   return error;
